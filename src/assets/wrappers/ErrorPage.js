e6615acaaa2760915dea7829447d2b93c9f7627d
konyatomi <konyatomi16@gmail.com>
2025-08-06 08:21:17 +0200

    Error Page - CSS

--- src/assets/wrappers/ErrorPage.js
+++ src/assets/wrappers/ErrorPage.js
@@ -6,6 +6,7 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
   img {
     width: 90vw;
     max-width: 600px;
@@ -13,6 +14,7 @@ const Wrapper = styled.div`
     margin-bottom: 2rem;
     margin-top: -3rem;
   }
+
   h3 {
     margin-bottom: 0.5rem;
   }
@@ -20,9 +22,10 @@ const Wrapper = styled.div`
   p {
     line-height: 1.5;
     margin-top: 0.5rem;
-    margin-bottom: 1rem;
+    margin-bottom: 0.5rem;
     color: var(--grey-500);
   }
+
   a {
     color: var(--primary-500);
     text-transform: capitalize;
