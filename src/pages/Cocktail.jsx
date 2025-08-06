91a207229bcf3a38731db2b1216f073ec38720a9
konyatomi <konyatomi16@gmail.com>
2025-08-06 22:38:35 +0200

    Single Cocktail - Setup

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,4 +1,20 @@
+import { useLoaderData, Link } from 'react-router-dom';
+import axios from 'axios';
+import Wrapper from '../assets/wrappers/CocktailPage';
+
+const singleCocktailUrl =
+  'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+
+export const loader = async ({ params }) => {
+  const { id } = params;
+  const response = await axios.get(`${singleCocktailUrl}${id}`);
+
+  return { id, data };
+};
+
 const Cocktail = () => {
+  const { id, data } = useLoaderData();
+
   return <div>Cocktail</div>;
 };
 export default Cocktail;
