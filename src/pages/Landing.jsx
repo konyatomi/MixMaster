f08daeb5c9dfd2db5e88f6edc5eed5bd76649966
konyatomi <konyatomi16@gmail.com>
2025-08-07 12:05:48 +0200

    React Query - Landing Page Setup

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,6 +6,22 @@ import SearchForm from '../components/SearchForm';
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
+import { useQuery } from '@tanstack/react-query';
+
+const searchCocktailsQuery = (searchTerm) => {
+  return {
+    queryKey: ['search', searchTerm || 'all'],
+    queryFn: async () => {
+      // UPDATE !!!
+      // Default to 'a' if no search term is provided since API has changed
+      searchTerm = searchTerm || 'a';
+
+      const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+      return response.data.drinks;
+    },
+  };
+};
+
 export const loader = async ({ request }) => {
   const url = new URL(request.url);
   const searchTerm = url.searchParams.get('search') || '';
@@ -13,14 +29,17 @@ export const loader = async ({ request }) => {
   // Ha nincs keresési kifejezés, akkor alapértelmezetten 'a' betűvel keresünk,
   // de a searchTerm üres marad a keresőmező számára
   const query = searchTerm || 'a';
-  const response = await axios.get(`${cocktailSearchUrl}${query}`);
+  // const response = await axios.get(`${cocktailSearchUrl}${query}`);
 
-  return { drinks: response.data.drinks, searchTerm };
+  return { searchTerm };
 };
 
 const Landing = () => {
-  const { drinks, searchTerm } = useLoaderData();
-
+  const { searchTerm } = useLoaderData();
+  const { data: drinks, isLoading } = useQuery(
+    searchCocktailsQuery(searchTerm)
+  );
+  if (isLoading) return <h4>Loading...</h4>;
   return (
     <>
       <SearchForm searchTerm={searchTerm} />
