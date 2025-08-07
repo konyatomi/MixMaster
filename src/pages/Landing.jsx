865395b0c9e65c469deb16d09779a6161b02624a
konyatomi <konyatomi16@gmail.com>
2025-08-07 11:40:05 +0200

    SearchForm - Complete

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,9 +6,14 @@ import SearchForm from '../components/SearchForm';
 const cocktailSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
-export const loader = async () => {
-  const searchTerm = 'a';
-  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+export const loader = async ({ request }) => {
+  const url = new URL(request.url);
+  const searchTerm = url.searchParams.get('search') || '';
+
+  // Ha nincs keresési kifejezés, akkor alapértelmezetten 'a' betűvel keresünk,
+  // de a searchTerm üres marad a keresőmező számára
+  const query = searchTerm || 'a';
+  const response = await axios.get(`${cocktailSearchUrl}${query}`);
 
   return { drinks: response.data.drinks, searchTerm };
 };
