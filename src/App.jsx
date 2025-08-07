316a1879d0d43a17567b02bfcfcb686c2e1e30ec
konyatomi <konyatomi16@gmail.com>
2025-08-07 11:45:21 +0200

    React Query - Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { createBrowserRouter, Route, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import {
   About,
@@ -12,8 +14,15 @@ import {
 
 import { loader as landingLoader } from './pages/Landing.jsx';
 import { loader as singleCocktailLoader } from './pages/Cocktail.jsx';
-
 import { action as newsletterAction } from './pages/Newsletter.jsx';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+    },
+  },
+});
 
 const router = createBrowserRouter([
   {
@@ -49,6 +58,11 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 export default App;
